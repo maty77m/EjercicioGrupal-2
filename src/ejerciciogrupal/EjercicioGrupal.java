cd828c212c76cf31c5df524d6207556e921d12ce
FerMSz <fermsz.89@gmail.com>
2023-08-08 11:28:02 -0300

    Mi saludo

--- src/ejerciciogrupal/EjercicioGrupal.java
+++ src/ejerciciogrupal/EjercicioGrupal.java
@@ -11,6 +11,7 @@ public class EjercicioGrupal {
     public static void main(String[] args) {
         System.out.println("Hola, soy Maty");  
         System.out.println("Hola chiquis, May :) ");
+        System.out.println("Hola,Soy Fer");
     
     }
     
