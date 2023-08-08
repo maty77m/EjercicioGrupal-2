d2f34ef1c04fe8d15f23c97decfb9f9f686c9e6b
maty77m <ale77m@gmail.com>
2023-08-08 11:00:03 -0300

    Initial commit

---
+++ src/ejerciciogrupal/EjercicioGrupal.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejerciciogrupal;
+
+/**
+ *
+ * @author ale77
+ */
+public class EjercicioGrupal {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hola, soy Maty");    
+    
+    }
+    
+}
